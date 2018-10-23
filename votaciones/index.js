6ef3db3b0afdf3a6e66739dcfc699015c0b90e89
Rodolfo Martinez Vega <rodomar@outlook.com>
2018-10-22 19:29:04 -0500

    First week

---
+++ votaciones/index.js
@@ -0,0 +1,31 @@
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+var abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+
+var VotingContract = web3.eth.contract(abi);
+
+var contractInstance = VotingContract.at('0x4c9999f944e2d09a5e663428bf05b3616249eb10');
+
+var candidates = {"Lopez": "candidate-1", "Anaya": "candidate-2", "Meade": "candidate-3"}
+
+function voteForCandidate(candidate) {
+ var candidateName = $("#candidate").val();
+ try {
+ 
+  contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
+   
+   let div_id = candidates[candidateName];
+   $("#"+div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+  });
+ } catch (err) {
+ }
+}
+
+$(document).ready(function() {
+ var candidateNames = Object.keys(candidates);
+ for (var i = 0; i < candidateNames.length; i++) {
+  let name = candidateNames[i];
+  let val = contractInstance.totalVotesFor.call(name).toString()
+  $("#"+candidates[name]).html(val);
+ }
+});
